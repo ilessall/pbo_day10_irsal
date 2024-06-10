dd70709e41247c7e957e1d0eebd411d9bd241f62
A-8 <A-8@A-8>
2024-06-10 10:51:48 +0700

    push

---
+++ pbo_day10_irsal/src/main/java/oopPraktek/MainGame.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oopPraktek;
+
+import java.util.Random;
+/**
+ *
+ * @author A-8
+ */
+public class MainGame {
+    public static void main(String[] args) {
+    // Create a new game object
+    Game game = new Game();
+    // Start the game
+    game.play();
+
+    }
+}
